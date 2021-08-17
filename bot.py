d55dbfa6ae15c34227282498f4cb9468f67f31ac
AB027897 <AB027897@students.nutleyschools.org>
2021-08-17 17:18:36 -0400

    First commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+load_dotenv()
+TOKEN = os.environ.get("DISCORD_TOKEN")
+bot = commands.Bot(command_prefix='!')
+
+
+client = discord.Client()
+
+
+@bot.event
+async def on_ready():
+    print(f' {client.user} has connected!')
+
+
+@client.event
+async def on_message(message):
+    if message.content == "1":
+        response = "Hello"
+        await message.channel.send(response)
+bot.run(TOKEN)
